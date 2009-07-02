32a287716fc9a9706793c95cf71e2ad16e7909ec
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-07-03 08:34:50 +0900

    initial commit

---
+++ lib/Test/Requires.pm
@@ -0,0 +1,33 @@
+package Test::Requires;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Requires -
+
+=head1 SYNOPSIS
+
+  use Test::Requires;
+
+=head1 DESCRIPTION
+
+Test::Requires is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom @*(#RJKLFHFSDLJF gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
